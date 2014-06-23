3ba3f9631a785a116e609cf8506b8b66e600c3e1
Brandon Swanson <github@brandonswanson.com>
2014-06-22 21:56:09 -0700

    functions shelled

--- grades.cpp
+++ grades.cpp
@@ -8,7 +8,7 @@
 * Overview:After inputing grade percentages caclulates average percent and
 *	   gives a total letter grade
 *
-* Input:A seris of percentages
+* Input:A seris of points out of total points
 *
 * Output:"Your Average percent is:%% for a grade of:AA"
 *
@@ -16,17 +16,71 @@
 
 #include <iostream>
 
+//global variables
+float pointsEarnedTotal=0;
+float pointsAvailableTotal=0;
+const std::string ERROR = "E";
+
+//Prompts user to input points earned
+void getGrades(int numGrades);
+
+//returns a letter grade equivalent after calculating a percentage
+std::string convertToLetterGrade(float pointsEarnedTotal,
+                                 float pointsAvailableTotal);
+
+
 int main(){
 	
-	std::string input;
+    //local variables
+	int numGrades;
+    std::string letterGrade;
+    float percentEarnedTotal;
+    
 
 	//Program Description and Input Prompt
 	std::cout << "This program will calculate your average percent score on your class assignments and award you a Letter Grade" << std::endl;
-	std::cout << "Please enter your the percentage you scored on each assignment followed by the return key" << std::endl;
-	std::cin>>input;
-	std::cout<<std::endl<<input<<std::endl;	
+	std::cout << "How many assignments did you have?";
+    std::cin >> numGrades;
+    
+    getGrades(numGrades);
+    letterGrade=convertToLetterGrade(98,99);
 
-
+    std::cout << letterGrade;
 
 	return 0;
 }
+
+/**************************************************************
+* Entry:a quantity of grades to be inputed
+*
+*Exit:Global variables pointsEarnedTotal and pointsAvailable totol
+*     will be incremented acording to user input
+*
+* Purpose:Retrieve points earned on assignments for the calculaiton
+*           of a letter grade
+*
+ ***************************************************************/
+void getGrades(int numGrades){
+    std::cout<<"num grades is" << numGrades << std::endl;
+}
+
+/**************************************************************
+* Entry:
+*
+*Exit:Will return a grade on scale A,B,C,D,F unless:
+*       -PointsEarnedTotal>PointsAvailableTotal  OR
+*       -PointsAvailableTotal = 0
+*       in wich case it will return an Error Code
+*
+* Purpose:To convert pointsEarned and PointsAvailable to a Letter Grade
+*
+ ***************************************************************/
+std::string convertToLetterGrade(float pointsEarnedTotal,
+                                 float pointsAvailableTotal){
+    std::cout<<"points earned is" << pointsEarnedTotal << std::endl;
+    std::cout<<"points total is" << pointsAvailableTotal << std::endl;
+    
+    return "A+";
+}
+
+
