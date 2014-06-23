3c33eebb078ffbb29a1892e292cb12bfa6c64b22
Brandon Swanson <github@brandonswanson.com>
2014-06-22 23:00:52 -0700

    functions working

--- grades.cpp
+++ grades.cpp
@@ -15,17 +15,22 @@
 ***********************************************************/
 
 #include <iostream>
+using namespace std;
+
 
 //global variables
 float pointsEarnedTotal=0;
 float pointsAvailableTotal=0;
-const std::string ERROR = "E";
+float percentEarned;
+const string ERROR = "E";
+const int EQUAL = 0; //defined by string.compare()
+
 
 //Prompts user to input points earned
 void getGrades(int numGrades);
 
 //returns a letter grade equivalent after calculating a percentage
-std::string convertToLetterGrade(float pointsEarnedTotal,
+string convertToLetterGrade(float pointsEarnedTotal,
                                  float pointsAvailableTotal);
 
 
@@ -33,19 +38,34 @@ int main(){
 	
     //local variables
 	int numGrades;
-    std::string letterGrade;
+    string letterGrade;
     float percentEarnedTotal;
     
+    // Only display two places after the decimal point,
+    // using the "magic formula" form Savitch, pg. 31
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
 
 	//Program Description and Input Prompt
-	std::cout << "This program will calculate your average percent score on your class assignments and award you a Letter Grade" << std::endl;
-	std::cout << "How many assignments did you have?";
-    std::cin >> numGrades;
+	cout << "This program will calculate your average percent score on your class assignments and award you a Letter Grade" << endl;
+	cout << "How many assignments did you have?";
+    cin >> numGrades;
     
     getGrades(numGrades);
     letterGrade=convertToLetterGrade(pointsEarnedTotal,pointsAvailableTotal);
 
-    std::cout << letterGrade;
+    //check for error
+    if (letterGrade.compare(ERROR) == EQUAL) {
+        cout << "something has gone wrong please launch program again" << endl;
+        return -1;
+    } else {
+        cout << "You earned a total of " << pointsEarnedTotal << " out of ";
+        cout << pointsAvailableTotal << " available points" << endl;
+        cout << "for a total of ";
+        cout << percentEarned*100 << " percent and a grade of: ";
+        cout << letterGrade << endl;
+    }
 
 	return 0;
 }
@@ -65,17 +85,17 @@ void getGrades(int numGrades){
     float pointsEarned;
     float pointsAvailable;
     
-    for (int i=1; i<=numGrades; i++) {
+    for (int i = 1; i <= numGrades; i++) {
         //retrive points earned and available for each assignment
-        std::cout << std::endl;
-        std::cout << "On assignment " << i << " you earned how many points:";
-        std::cin >> pointsEarned;
-        std::cout << "  out of how many points available:";
-        std::cin >> pointsAvailable;
+        cout << endl;
+        cout << "On assignment " << i << " you earned how many points:";
+        cin >> pointsEarned;
+        cout << "  out of how many points available:";
+        cin >> pointsAvailable;
         
         //incrememt totals
-        pointsEarnedTotal+=pointsEarned;
-        pointsAvailableTotal+=pointsAvailable;
+        pointsEarnedTotal += pointsEarned;
+        pointsAvailableTotal += pointsAvailable;
     }
 }
 
@@ -90,12 +110,49 @@ void getGrades(int numGrades){
 * Purpose:To convert pointsEarned and PointsAvailable to a Letter Grade
 *
  ***************************************************************/
-std::string convertToLetterGrade(float pointsEarnedTotal,
+string convertToLetterGrade(float pointsEarnedTotal,
                                  float pointsAvailableTotal){
-    std::cout<<"points earned is" << pointsEarnedTotal << std::endl;
-    std::cout<<"points total is" << pointsAvailableTotal << std::endl;
+    //calculate percent
+    if (pointsAvailableTotal == 0  || pointsAvailableTotal<pointsEarnedTotal) {
+        return ERROR;
+    } else {
+        percentEarned = pointsEarnedTotal / pointsAvailableTotal;
+    }
     
-    return "A+";
+    //covert to letter grade
+    if (percentEarned < 0 || percentEarned > 1) {
+       return ERROR
+    }else if (percentEarned>=98){
+        return "A+";
+    }else if (percentEarned>=92){
+        return "A";
+    }else if (percentEarned>=90){
+        return "A-";
+    }else if (percentEarned>=88){
+        return "B+";
+    }else if (percentEarned>=82){
+        return "B";
+    }else if (percentEarned>=80){
+        return "B-";
+    }else if (percentEarned>=78){
+        return "C+";
+    }else if (percentEarned>=72){
+        return "C";
+    }else if (percentEarned>=70){
+        return "C-";
+    }else if (percentEarned>=68){
+        return "D+";
+    }else if (percentEarned>=62){
+        return "D";
+    }else if (percentEarned>=60){
+        return "D-";
+    }else if (percentEarned<60){
+        return "F";
+    }
+    
+    
+    return ERROR;  //this line should never be reached,
+                   //but we will return nevertheless
 }
 
 
